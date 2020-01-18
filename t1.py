d1f405f84d89f372869140c1ee52dbcd568ad3ed
isaiasPb21 <57261898+isaiasPb21@users.noreply.github.com>
2020-01-18 12:02:27 -0300

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 5, 20, 3, 1, 8]
+y = [2, 3, 7, 6, 5, 12, 15]
+
+# Titulo
+plt.title("Scatterplot gráfico")
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# Plota o gráfico de "Pontos"
+plt.scatter(x, y, label = "Meus pontos", color = "#0000ff")
+# Criando uma linha sobre os pontos, "ligando os pontos" // Linha ligando os pontos
+plt.plot(x, y, color = "#00ff00") 
+plt.legend()
+
+plt.show()
